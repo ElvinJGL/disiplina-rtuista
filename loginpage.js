b6e3733a504fe6387f57d06ec1f63f428f6205f3
ElvinJGL <landichoelvin@gmail.com>
2025-02-19 23:47:24 +0800

    fifth commit

---
+++ loginpage.js
@@ -0,0 +1,21 @@
+// document.querySelector("#login_form").addEventListener("submit",function(e){
+//     e.preventDefault()
+//     let username = document.querySelector("#login_form input[name='user_name']").value;
+//     let password = document.querySelector("#login_form input[name='password']").value;
+    
+//     let user_name_validator =  document.querySelector("#user_name_validator")
+//     let password_validator =  document.querySelector("#password_validator")
+
+//     if (!username){
+//         user_name_validator.innerHTML = "Username is Requried"
+//     }
+//     else{
+//         user_name_validator.innerHTML = ""
+//     }
+//     if (!password){
+//         password_validator.innerHTML = "Password is Requried"
+//     }
+//     else{
+//         password_validator.innerHTML = ""
+//     }
+// })
